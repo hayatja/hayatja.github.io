e55187f19e91c93e9e08cac670e909c40830b5d7
hayatja <hjami014@gmail.com>
2024-08-26 15:39:12 -0400

    fixed button-content issue and updated resume

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,34 +1,41 @@
+import React from "react";
 import { TypeAnimation } from "react-type-animation";
 
 const Title = () => {
   return (
-    <TypeAnimation
-      sequence={[
-        "an electrical engineer.",
-        2000,
-        "a computer scientist.",
-        2000,
-        "a developer.",
-        2000,
-        "a student.",
-        2000,
-      ]}
-      wrapper="span"
-      className="title-animation" // Add this line
-      speed={50}
-      style={{
-        fontWeight: "700",
-        fontSize: "3em",
-        fontFamily: "helvetica",
-        textDecoration: "underline",
-        background: "#e4ddeb",
-        paddingInline: "20px",
-        paddingTop: "10px",
-        paddingBottom: "10px",
-        borderRadius: "10px",
-      }}
-      repeat={Infinity}
-    />
+    <div className="title-container" style={{ 
+      position: 'fixed', // ensures title is in fixed viewpoint
+      left: '50%', 
+      transform: 'translateX(-50%)' 
+      }}>
+      <TypeAnimation
+        sequence={[
+          "an electrical engineer.",
+          2000,
+          "a computer scientist.",
+          2000,
+          "a developer.",
+          2000,
+          "a student.",
+          2000,
+        ]}
+        wrapper="span"
+        className="title-animation"
+        speed={50}
+        style={{
+          fontWeight: "700",
+          fontSize: "3em",
+          fontFamily: "helvetica",
+          textDecoration: "underline",
+          background: "#e4ddeb",
+          paddingInline: "20px",
+          paddingTop: "10px",
+          paddingBottom: "10px",
+          borderRadius: "10px",
+        }}
+        repeat={Infinity}
+      />
+    </div>
   );
 };
 
